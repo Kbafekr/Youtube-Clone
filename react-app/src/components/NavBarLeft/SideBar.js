ca14769e64dcf56b4dcdb242681a330fdcdc35f0
Keanen <KeanenBafekr@me.com>
2022-10-25 19:38:30 -0400

    setup heroku

--- react-app/src/components/NavBarLeft/SideBar.js
+++ react-app/src/components/NavBarLeft/SideBar.js
@@ -6,23 +6,24 @@ const SideBarNav = () => {
   return (
     <nav className="SideNavBarContainer">
       <div className="InternalSideBarContainer">
-      <NavLink to="/" exact={true} activeClassName="active">
-        Home
-      </NavLink>
-
-      <NavLink to="/login" exact={true} activeClassName="active">
-        Login
-      </NavLink>
-
-      <NavLink to="/sign-up" exact={true} activeClassName="active">
-        Sign Up
-      </NavLink>
-
-      <NavLink to="/users" exact={true} activeClassName="active">
-        Users
-      </NavLink>
-
-      <LogoutButton />
+      <div>
+      comments
+      </div>
+      <div>
+      comments
+      </div>
+      <div>
+      comments
+      </div>
+      <div>
+      comments
+      </div>
+      <div>
+      comments
+      </div>
+      <div>
+      comments
+      </div>
       </div>
     </nav>
   );
