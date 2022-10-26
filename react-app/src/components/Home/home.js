b858605dd3319fe2144f1518f77dab790feac458
Keanen <KeanenBafekr@me.com>
2022-10-26 11:19:21 -0400

    css

--- react-app/src/components/Home/home.js
+++ react-app/src/components/Home/home.js
@@ -1,10 +1,22 @@
 import "./home.css";
+import { useSelector } from "react-redux";
+import { useState, useEffect } from "react";
+import { getAllVideosThunk } from "../../store/video";
+import { useDispatch } from "react-redux";
+
 
 export function HomePage() {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getAllVideosThunk());
+  }, [dispatch])
   return (
     <>
       <div className="homeContainer">
+        <div className="homeContainerInner">
         <h1>Welcome to You2ube</h1>
+        </div>
       </div>
     </>
   );
