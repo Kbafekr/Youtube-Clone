7c99491209d3b712d942497916c4f44b86fb8501
Keanen <KeanenBafekr@me.com>
2022-10-28 10:00:00 -0400

    Update User.js

--- react-app/src/components/User.js
+++ react-app/src/components/User.js
@@ -1,46 +1,48 @@
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import './User.css'
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import "./User.css";
 
-function User({sidePanel}) {
-  const [user, setUser] = useState({});
-  const { userId }  = useParams();
+function User({ sidePanel }) {
+  const { userId } = useParams();
+  const currentUser = useSelector((state) => state.session.user);
 
-  useEffect(() => {
-    if (!userId) {
-      return;
-    }
-    (async () => {
-      const response = await fetch(`/api/users/${userId}`);
-      const user = await response.json();
-      setUser(user);
-    })();
-  }, [userId]);
-
-  if (!user) {
-    return null;
-  }
-
-  return (
-    <>
-     <div className={sidePanel == true ? "homeContainer" : "homeContainerClosed"}>
-
-    <ul>
-      <li>
-        <strong>User Id</strong> {userId}
-      </li>
-      <li>
-        <strong>First Name</strong> {user.first_name}
-      </li>
-      <li>
-        <strong>Last Name</strong> {user.last_name}
-      </li>
-      <li>
-        <strong>Email</strong> {user.email}
-      </li>
-    </ul>
-     </div>
-     </>
-  );
+  if (userId == currentUser.id) {
+    return (
+      <>
+        <div
+          className={
+            sidePanel == true ? "homeContainer" : "homeContainerClosed"
+          }
+        >
+          <ul>
+            <li>
+              <strong>User Id</strong> {userId}
+            </li>
+            <li>
+              <strong>First Name</strong> {currentUser.first_name}
+            </li>
+            <li>
+              <strong>Last Name</strong> {currentUser.last_name}
+            </li>
+            <li>
+              <strong>Email</strong> {currentUser.email}
+            </li>
+          </ul>
+        </div>
+      </>
+    );
+  } else
+    return (
+      <>
+        <div
+          className={
+            sidePanel == true ? "homeContainer" : "homeContainerClosed"
+          }
+        >
+          <div>You are unauthorized to view this page</div>
+        </div>
+      </>
+    );
 }
 export default User;
