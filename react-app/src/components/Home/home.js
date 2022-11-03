3f437332dc2d3a982d7ee5c01b77effffb99c825
Keanen <KeanenBafekr@me.com>
2022-11-03 08:50:24 -0400

    tags in home page

--- react-app/src/components/Home/home.js
+++ react-app/src/components/Home/home.js
@@ -21,8 +21,10 @@ export function HomePage({ sidePanel }) {
   const history = useHistory();
   const user = useSelector((state) => state.session.user);
   const videos = useSelector((state) => state.video);
+  const tags = useSelector((state) => state.tags);
   const channels = useSelector((state) => state.channel);
   const [newChannelMade, setNewChannelMade] = useState(false);
+  const [tagsFilter, setTagsFilter] = useState("all")
 
   const email =
     "fsdaiufgh3w9832f23wkjqfhwejkfasdbff9843wqeyrwdjkafhsdf@gmail.com";
@@ -30,9 +32,11 @@ export function HomePage({ sidePanel }) {
 
   let videosArray;
   let channelsArray;
+  let tagsArray;
 
   videosArray = Object.values(videos);
   channelsArray = Object.values(channels);
+  tagsArray = Object.values(tags)
 
   useEffect(() => {
     dispatch(getAllChannelsThunk());
@@ -87,6 +91,57 @@ export function HomePage({ sidePanel }) {
     }
   }, [dispatch, channelsArray, user]);
 
+  let arrayResult;
+  let videoArrayCopy;
+  let sortedVideosByNewest;
+
+  if (videosArray) {
+    videoArrayCopy = [...videosArray]
+  }
+  if (videoArrayCopy && videoArrayCopy.length > 0) {
+    sortedVideosByNewest = videoArrayCopy.sort((a, b) => b.id - a.id);
+  }
+
+  if (tagsFilter == "New") {
+    arrayResult = [...sortedVideosByNewest]
+  }
+  else if (tagsFilter == "Gaming") {
+    let filteredTags = tagsArray.filter((tag) => tag.videoId)
+    arrayResult = [...sortedVideosByNewest]
+  }
+  else if (tagsFilter == "Music") {
+    arrayResult = videosArray.filter((video) => video.tags)
+  }
+  else if (tagsFilter == "Sports") {
+    arrayResult = [...sortedVideosByNewest]
+  }
+  else if (tagsFilter == "Nature") {
+    arrayResult = [...sortedVideosByNewest]
+  }
+  else if (tagsFilter == "Superheroes") {
+    arrayResult = [...sortedVideosByNewest]
+  }
+  else if (tagsFilter == "Anime") {
+    arrayResult = [...sortedVideosByNewest]
+  }
+  else if (tagsFilter == "Programming") {
+    arrayResult = [...sortedVideosByNewest]
+  }
+  else if (tagsFilter == "News") {
+    arrayResult = [...sortedVideosByNewest]
+  }
+  else if (tagsFilter == "Podcasts") {
+    arrayResult = [...sortedVideosByNewest]
+  }
+  else if (tagsFilter == "Food") {
+    arrayResult = [...sortedVideosByNewest]
+  }
+  else if (tagsFilter == "TV") {
+    arrayResult = [...sortedVideosByNewest]
+  }
+  else {
+    arrayResult = [...videosArray]
+  }
   return (
     <>
       <div
@@ -95,25 +150,24 @@ export function HomePage({ sidePanel }) {
         <div className="homeContainerInner">
           <div className="homeTagsBarContainer">
             <div className="homeTagsBar">
-              <div className="tagHomePageActive">All</div>
-              <div className="tagHomePage">New</div>
-              <div className="tagHomePage">Gaming</div>
-              <div className="tagHomePage">Music</div>
-              <div className="tagHomePage">Sports</div>
-              <div className="tagHomePage">Nature</div>
-              <div className="tagHomePage">Superheroes</div>
-              <div className="tagHomePage">Anime</div>
-              <div className="tagHomePage">Programmming</div>
-              <div className="tagHomePage">News</div>
-              <div className="tagHomePage">Podcasts</div>
-              <div className="tagHomePage">Food</div>
-              <div className="tagHomePage">Scenes</div>
-              <div className="tagHomePage">Fashion</div>
+              <div className={tagsFilter == "All" ? "tagHomePageActive" : "tagHomePage"} onClick={() => setTagsFilter("All")}>All</div>
+              <div className={tagsFilter == "New" ? "NewtagHomePageActive" : "tagHomePage"} onClick={() => setTagsFilter("New")}>New to you</div>
+              <div className={tagsFilter == "Gaming" ? "tagHomePageActive" : "tagHomePage"} onClick={() => setTagsFilter("Gaming")}>Gaming</div>
+              <div className={tagsFilter == "Music" ? "tagHomePageActive" : "tagHomePage"} onClick={() => setTagsFilter("Music")}>Music</div>
+              <div className={tagsFilter == "Sports" ? "tagHomePageActive" : "tagHomePage"} onClick={() => setTagsFilter("Sports")}>Sports</div>
+              <div className={tagsFilter == "Nature" ? "tagHomePageActive" : "tagHomePage"} onClick={() => setTagsFilter("Nature")}>Nature</div>
+              <div className={tagsFilter == "Superheroes" ? "tagHomePageActive" : "tagHomePage"} onClick={() => setTagsFilter("Superheroes")}>Superheroes</div>
+              <div className={tagsFilter == "Anime" ? "tagHomePageActive" : "tagHomePage"} onClick={() => setTagsFilter("Anime")}>Anime</div>
+              <div className={tagsFilter == "Programming" ? "tagHomePageActive" : "tagHomePage"} onClick={() => setTagsFilter("Programming")}>Programming</div>
+              <div className={tagsFilter == "News" ? "tagHomePageActive" : "tagHomePage"} onClick={() => setTagsFilter("News")}>News</div>
+              <div className={tagsFilter == "Podcasts" ? "tagHomePageActive" : "tagHomePage"} onClick={() => setTagsFilter("Podcasts")}>Podcasts</div>
+              <div className={tagsFilter == "Food" ? "tagHomePageActive" : "tagHomePage"} onClick={() => setTagsFilter("Food")}>Food</div>
+              <div className={tagsFilter == "TV" ? "tagHomePageActive" : "tagHomePage"} onClick={() => setTagsFilter("TV")}>TV</div>
             </div>
           </div>
           <div className="VideosMapped">
-            {videosArray &&
-              videosArray.map((video) => {
+            {arrayResult &&
+              arrayResult.map((video) => {
                 return (
                   <>
                     <div className="VideoCardHome">
