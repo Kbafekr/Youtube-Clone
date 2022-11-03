cb5c34bd91d98be2a26dbf3ef14c922217ce66da
Keanen <KeanenBafekr@me.com>
2022-11-02 21:03:55 -0400

    Update Comments.js

--- react-app/src/components/PlayVideo/CommentsSection/Comments.js
+++ react-app/src/components/PlayVideo/CommentsSection/Comments.js
@@ -80,7 +80,6 @@ export function CommentsSection() {
     commentsArrayForMapping = [...filteredCommentsArray]
   }
 
-  console.log(commentsArrayForMapping)
   return (
     <>
       <div className="CommentsLengthAndSort">
