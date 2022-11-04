6a94d17b019418e2738c1d54efec005b94daaa17
Keanen <KeanenBafekr@me.com>
2022-11-04 12:48:23 -0400

    updated seeders for channels and users

--- app/seeds/channels.py
+++ app/seeds/channels.py
@@ -12,15 +12,81 @@ def seed_channels():
         profile_picture="https://64.media.tumblr.com/06cb8dbb8be14d91777d2f9e16f6a407/012792e20c1bc675-b2/s500x750/565f42f875a135af7efbebc1f662071b11a64bda.jpg",
         banner_picture='https://img.freepik.com/premium-vector/dark-navy-blue-banner-background-vector-abstract-graphic-design-banner-pattern-background-template_181182-18817.jpg')
     channel_3 = Channel(
-        channel_name='DemoUserGaming', user_id='2',
-        profile_picture="https://static1.thegamerimages.com/wordpress/wp-content/uploads/2022/01/Monkey.png",
-        banner_picture='https://i.pinimg.com/736x/f2/40/67/f24067d46bbcf1669bf54900feb40092.jpg')
+        channel_name='OnePiece', user_id='2',
+        profile_picture="https://i.ytimg.com/vi/xDeQP5Y7Tus/maxresdefault.jpg",
+        banner_picture='https://i.redd.it/2gavdohhcozz.jpg')
     channel_4 = Channel(
-        channel_name='DemoUserGaming', user_id='3',
-        profile_picture="https://static1.thegamerimages.com/wordpress/wp-content/uploads/2022/01/Emo.png",
-        banner_picture='https://i.ytimg.com/vi/g6klk13VlOI/maxresdefault.jpg')
+        channel_name='Admirals', user_id='2',
+        profile_picture="https://i1.sndcdn.com/avatars-000090559018-l7pk9f-t500x500.jpg",
+        banner_picture='https://i.pinimg.com/736x/8f/b6/b9/8fb6b9d1829a44e45416e8f31e08dc42.jpg')
+    channel_5 = Channel(
+        channel_name='Bondman', user_id='3',
+        profile_picture="https://static1.cbrimages.com/wordpress/wp-content/uploads/2021/12/spy-x-family-bond-image.jpg",
+        banner_picture='https://img1.ak.crunchyroll.com/i/spire3/a5708621d3b23af120d50d9c7965d86b1637343001_main.png')
+    channel_6 = Channel(
+        channel_name='WakuWaku', user_id='3',
+        profile_picture="https://i.kym-cdn.com/entries/icons/mobile/000/040/241/anya_heh_thumbnail.jpg",
+        banner_picture='https://goodsrepublic.com/media/binary/007/261/122/7261122.jpg')
+    channel_7 = Channel(
+        channel_name='Markiplier', user_id='4',
+        profile_picture="https://i.pinimg.com/originals/ee/2d/29/ee2d292c7ebcae49f47a1464659422c0.png",
+        banner_picture='https://looxcie.com/wp-content/uploads/2018/12/markiplier-thumbnail.png')
+    channel_8 = Channel(
+        channel_name='IGN', user_id='4',
+        profile_picture="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/IGN_Entertainment_Logo.svg/1200px-IGN_Entertainment_Logo.svg.png",
+        banner_picture='https://www.pngkey.com/png/detail/21-214887_banner-black-and-white-library-deepfreeze-ign-ign.png')
+    channel_9 = Channel(
+        channel_name='Politico', user_id='5',
+        profile_picture="https://cdn.comparably.com/24749373/l/163465/logo.jpg",
+        banner_picture='https://pbs.twimg.com/media/CdFGLcxWwAA9932.jpg')
+    channel_10 = Channel(
+        channel_name='FashionShow', user_id='5',
+        profile_picture="https://www.onlinelogomaker.com/blog/wp-content/uploads/2017/09/fashion-logo-design.jpg",
+        banner_picture="https://marketplace.canva.com/EAFGKRRskMs/1/0/1600w/canva-brown-and-beige-minimalist-fashion-banner-lYcbGpUSVGo.jpg")
+    channel_11 = Channel(
+        channel_name='CrashCourse', user_id='6',
+        profile_picture="https://upload.wikimedia.org/wikipedia/en/e/e9/Crash_Course_logo.png",
+        banner_picture='https://www.clipartmax.com/png/middle/261-2612453_crash-course-website-crash-course-world-history-logo.png')
+    channel_12 = Channel(
+        channel_name='Fireship', user_id='6',
+        profile_picture="https://dslv9ilpbe7p1.cloudfront.net/JQ6paMWlouqHZYm89mG-fA_store_logo_image.png",
+        banner_picture='https://i.ytimg.com/vi/gun8OiGtlNc/maxresdefault.jpg')
+    channel_13 = Channel(
+        channel_name='Tasty', user_id='7',
+        profile_picture="https://www.licensingmattersglobal.com/wp-content/uploads/2019/03/News-Tasty.jpg",
+        banner_picture='https://brandingstyleguides.com/wp-content/guidelines/2019/10/Tasty_Style_Guide_032317-1-1000x563.jpg')
+    channel_14 = Channel(
+        channel_name='WatchMojo', user_id='7',
+        profile_picture="https://credobags.com/wp-content/uploads/2017/11/watchmojo-media-logo.jpg",
+        banner_picture='https://static.tvtropes.org/pmwiki/pub/images/watchmojobetter.png')
+    channel_15 = Channel(
+        channel_name='Marvel', user_id='8',
+        profile_picture="https://i.pinimg.com/originals/5c/1d/f8/5c1df865ec798229c9a722c6240a896f.jpg",
+        banner_picture='https://terrigen-cdn-dev.marvel.com/content/prod/1x/marvelstudios_logo.jpg')
+    channel_16 = Channel(
+        channel_name='HBOMax', user_id='8',
+        profile_picture="https://cdn.sanity.io/images/1pn9obcz/production/6c440f28ca5d1a16e74289dd57cc0ae1167eb446-1920x1080.jpg",
+        banner_picture='https://logos-world.net/wp-content/uploads/2022/01/HBO-Max-Emblem.png')
+    channel_17 = Channel(
+        channel_name='National Geographics', user_id='9',
+        profile_picture="https://i.pinimg.com/originals/66/d0/11/66d0115c90e3325a5372aeb37d0f1728.jpg",
+        banner_picture='https://1000logos.net/wp-content/uploads/2017/04/national-geographic-symbol.jpg')
+    channel_18 = Channel(
+        channel_name='First We Feast', user_id='9',
+        profile_picture="https://i.pinimg.com/474x/ee/bd/df/eebddf27ea217996819b946de3b2bfee.jpg",
+        banner_picture='https://images.firstwefeast.com/complex/image/upload/f_auto,fl_lossy,q_auto,w_1200/firstwefeast-logo_o3d1gq.jpg')
+    channel_19 = Channel(
+        channel_name='Really Slow Motion', user_id='10',
+        profile_picture="https://lastfm.freetls.fastly.net/i/u/ar0/9c574089a88c44b5c4a45e7af8828973.jpg",
+        banner_picture='https://i.ytimg.com/vi/0FWDMki6Pp8/maxresdefault.jpg')
+    channel_20 = Channel(
+        channel_name='Insider', user_id='10',
+        profile_picture="https://i.pinimg.com/originals/ce/7b/ac/ce7bacb704a4d54550e285ed89e1bb82.jpg",
+        banner_picture='https://www.insider.com/public/assets/INSIDER/US/og-image-logo-social.png')
 
-    channels = [channel_1, channel_2, channel_3, channel_4]
+    channels = [channel_1, channel_2, channel_3, channel_4, channel_5, channel_6, channel_7, channel_8, channel_9,
+    channel_10, channel_11, channel_12, channel_13, channel_14, channel_15,
+    channel_16, channel_17, channel_18, channel_19, channel_20]
     for channel in channels:
         db.session.add(channel)
         db.session.commit()
--- app/seeds/users.py
+++ app/seeds/users.py
@@ -6,11 +6,25 @@ def seed_users():
     demo = User(
         first_name='Demo', last_name='User', email='demo@aa.io', active_channel=1, password='password')
     marnie = User(
-        first_name='Marnie', last_name='Malkovich', email='marnie@aa.io', active_channel=3, password='password')
+        first_name='Borsalino', last_name='Kizaru', email='marnie@aa.io', active_channel=3, password='password')
     bobbie = User(
-        first_name='Bobbie', last_name='Bobbert', email='bobbie@aa.io', active_channel=4, password='password')
+        first_name='Anya', last_name='Forger', email='bobbie@aa.io', active_channel=5, password='password')
+    Marc = User(
+        first_name='Marc', last_name='Hernandez', email='MarcHernandez@aa.io', active_channel=7, password='password')
+    Gev = User(
+        first_name='Gev', last_name='Apikyan', email='GevApikyan@aa.io', active_channel=9, password='password')
+    Amy = User(
+        first_name='Amy', last_name='Bennett', email='AmyBennett@aa.io', active_channel=11, password='password')
+    kermit = User(
+        first_name='Carl', last_name='Wheezer', email='bobbie@aa.io', active_channel=13, password='password')
+    bruce = User(
+        first_name='Bruce', last_name='wayne', email='brucewayne@aa.io', active_channel=15, password='password')
+    dog = User(
+        first_name='Nikita', last_name='doggo', email='Nikita@aa.io', active_channel=17, password='password')
+    cindy = User(
+        first_name='Cyndaquil', last_name='pokemon', email='pokemon@aa.io', active_channel=19, password='password')
 
-    users = [demo, marnie, bobbie]
+    users = [demo, marnie, bobbie, Marc, Gev, Amy, kermit, bruce, dog, cindy]
     for user in users:
         db.session.add(user)
         db.session.commit()
