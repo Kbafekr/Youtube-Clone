1048b2ec763f9891852eb2fae2d727bc19ea7751
Keanen <KeanenBafekr@me.com>
2022-11-04 14:05:41 -0400

    seeders for tags and videos

--- app/seeds/tags.py
+++ app/seeds/tags.py
@@ -3,16 +3,138 @@ from app.models import db, Tag
 # Adds a demo user, you can add other users here if you want
 def seed_tags():
 
-    tag_1 = Tag(channel_id=1, video_id=1, body="Marvel")
-    tag_2 = Tag(channel_id=1, video_id=1, body="SuperHero")
-    tag_3 = Tag(channel_id=1, video_id=1, body="Ant-man")
-    tag_4 = Tag(channel_id=1, video_id=1, body="Heist")
-    tag_5 = Tag(channel_id=1, video_id=4, body="Nirvana")
-    tag_6 = Tag(channel_id=1, video_id=4, body="Music")
-    tag_7 = Tag(channel_id=1, video_id=4, body="Grunge")
-    tag_8 = Tag(channel_id=1, video_id=4, body="Rock")
+    tag_1 = Tag(channel_id=15, video_id=1, body="Marvel")
+    tag_2 = Tag(channel_id=15, video_id=1, body="SuperHero")
+    tag_3 = Tag(channel_id=15, video_id=1, body="Ant-man")
+    tag_4 = Tag(channel_id=15, video_id=1, body="Heist")
+    tag_5 = Tag(channel_id=15, video_id=1, body="Trailer")
 
-    tags = [tag_1, tag_2, tag_3, tag_4, tag_5, tag_6, tag_7, tag_8]
+    tag_6 = Tag(channel_id=1, video_id=2, body="Show")
+    tag_7 = Tag(channel_id=1, video_id=2, body="Dragon")
+    tag_8 = Tag(channel_id=1, video_id=2, body="Drama")
+    tag_9 = Tag(channel_id=1, video_id=2, body="Scene")
+    tag_10 = Tag(channel_id=1, video_id=2, body="HBO")
+
+    tag_11 = Tag(channel_id=3, video_id=3, body="Anime")
+    tag_12 = Tag(channel_id=3, video_id=3, body="Japan")
+    tag_13 = Tag(channel_id=3, video_id=3, body="Animator")
+    tag_14 = Tag(channel_id=3, video_id=3, body="Education")
+    tag_15 = Tag(channel_id=3, video_id=3, body="Learning")
+
+    tag_16 = Tag(channel_id=2, video_id=4, body="Nirvana")
+    tag_17 = Tag(channel_id=2, video_id=4, body="Grunge")
+    tag_18 = Tag(channel_id=2, video_id=4, body="Rock")
+    tag_19 = Tag(channel_id=2, video_id=4, body="Remix")
+    tag_20 = Tag(channel_id=2, video_id=4, body="Music")
+
+    tag_21 = Tag(channel_id=10, video_id=5, body="Interview")
+    tag_22 = Tag(channel_id=10, video_id=5, body="Acting")
+    tag_23 = Tag(channel_id=10, video_id=5, body="Fashion")
+    tag_24 = Tag(channel_id=10, video_id=5, body="Actor")
+    tag_25 = Tag(channel_id=10, video_id=5, body="Movie")
+
+    tag_26 = Tag(channel_id=12, video_id=6, body="Programming")
+    tag_27 = Tag(channel_id=12, video_id=6, body="Learning")
+    tag_28 = Tag(channel_id=12, video_id=6, body="Education")
+    tag_29 = Tag(channel_id=12, video_id=6, body="Coding")
+    tag_30 = Tag(channel_id=12, video_id=6, body="Software")
+
+    tag_31 = Tag(channel_id=15, video_id=7, body="Action")
+    tag_32 = Tag(channel_id=15, video_id=7, body="Movie")
+    tag_33 = Tag(channel_id=15, video_id=7, body="Trailer")
+    tag_34 = Tag(channel_id=15, video_id=7, body="Superhero")
+    tag_35 = Tag(channel_id=15, video_id=7, body="Marvel")
+
+    tag_36 = Tag(channel_id=1, video_id=8, body="Succession")
+    tag_37 = Tag(channel_id=1, video_id=8, body="Show")
+    tag_38 = Tag(channel_id=1, video_id=8, body="Scene")
+    tag_39 = Tag(channel_id=1, video_id=8, body="Drama")
+    tag_40 = Tag(channel_id=1, video_id=8, body="HBO")
+
+    tag_41 = Tag(channel_id=11, video_id=9, body="Education")
+    tag_42 = Tag(channel_id=11, video_id=9, body="Learning")
+    tag_43 = Tag(channel_id=11, video_id=9, body="Health")
+    tag_44 = Tag(channel_id=11, video_id=9, body="Habits")
+    tag_45 = Tag(channel_id=11, video_id=9, body="Stress")
+
+    tag_46 = Tag(channel_id=2, video_id=10, body="Cartoon")
+    tag_47 = Tag(channel_id=2, video_id=10, body="Show")
+    tag_48 = Tag(channel_id=2, video_id=10, body="Analysis")
+    tag_49 = Tag(channel_id=2, video_id=10, body="Podcast")
+    tag_50 = Tag(channel_id=2, video_id=10, body="Comedy")
+
+
+    tag_51 = Tag(channel_id=20, video_id=11, body="Nirvana")
+    tag_52 = Tag(channel_id=20, video_id=11, body="Electronic")
+    tag_53 = Tag(channel_id=20, video_id=11, body="Rock")
+    tag_54 = Tag(channel_id=20, video_id=11, body="Remix")
+    tag_55 = Tag(channel_id=20, video_id=11, body="Music")
+
+    tag_56 = Tag(channel_id=19, video_id=12, body="Electronic")
+    tag_57 = Tag(channel_id=19, video_id=12, body="Anime")
+    tag_58 = Tag(channel_id=19, video_id=12, body="Music")
+    tag_59 = Tag(channel_id=19, video_id=12, body="Dubstep")
+    tag_60 = Tag(channel_id=19, video_id=12, body="Animated")
+
+    tag_61 = Tag(channel_id=3, video_id=13, body="Sports")
+    tag_62 = Tag(channel_id=3, video_id=13, body="Soccer")
+    tag_63 = Tag(channel_id=3, video_id=13, body="Score")
+    tag_64 = Tag(channel_id=3, video_id=13, body="Game")
+    tag_65 = Tag(channel_id=3, video_id=13, body="UEFA")
+
+    tag_66 = Tag(channel_id=6, video_id=14, body="Podcast")
+    tag_67 = Tag(channel_id=6, video_id=14, body="H3")
+    tag_68 = Tag(channel_id=6, video_id=14, body="News")
+    tag_69 = Tag(channel_id=6, video_id=14, body="Fashion")
+    tag_70 = Tag(channel_id=6, video_id=14, body="Entertainment")
+
+    tag_71 = Tag(channel_id=9, video_id=15, body="News")
+    tag_72 = Tag(channel_id=9, video_id=15, body="Politics")
+    tag_73 = Tag(channel_id=9, video_id=15, body="International")
+    tag_74 = Tag(channel_id=9, video_id=15, body="Germany")
+    tag_75 = Tag(channel_id=9, video_id=15, body="Diplomacy")
+
+    tag_76 = Tag(channel_id=17, video_id=16, body="Nature")
+    tag_77 = Tag(channel_id=17, video_id=16, body="Animals")
+    tag_78 = Tag(channel_id=17, video_id=16, body="Wolves")
+    tag_79 = Tag(channel_id=17, video_id=16, body="Wild")
+    tag_80 = Tag(channel_id=17, video_id=16, body="Education")
+
+    tag_81 = Tag(channel_id=16, video_id=17, body="Scene")
+    tag_82 = Tag(channel_id=16, video_id=17, body="Sports")
+    tag_83 = Tag(channel_id=16, video_id=17, body="Movie")
+    tag_84 = Tag(channel_id=16, video_id=17, body="Baseball")
+    tag_85 = Tag(channel_id=16, video_id=17, body="Acting")
+
+    tag_86 = Tag(channel_id=13, video_id=18, body="Tasty")
+    tag_87 = Tag(channel_id=13, video_id=18, body="Food")
+    tag_88 = Tag(channel_id=13, video_id=18, body="Recipe")
+    tag_89 = Tag(channel_id=13, video_id=18, body="Ramen")
+    tag_90 = Tag(channel_id=13, video_id=18, body="Yum")
+
+    tag_91 = Tag(channel_id=4, video_id=19, body="Scene")
+    tag_92 = Tag(channel_id=4, video_id=19, body="HBO")
+    tag_93 = Tag(channel_id=4, video_id=19, body="Drama")
+    tag_94 = Tag(channel_id=4, video_id=19, body="Show")
+    tag_95 = Tag(channel_id=4, video_id=19, body="Acting")
+
+    tag_96 = Tag(channel_id=7, video_id=20, body="Gaming")
+    tag_97 = Tag(channel_id=7, video_id=20, body="COD")
+    tag_98 = Tag(channel_id=7, video_id=20, body="Walkthrough")
+    tag_99 = Tag(channel_id=7, video_id=20, body="Gameplay")
+    tag_100 = Tag(channel_id=7, video_id=20, body="Campaign")
+
+    tags = [tag_1, tag_2, tag_3, tag_4, tag_5, tag_6, tag_7, tag_8, tag_9, tag_10,
+    tag_11, tag_12, tag_13, tag_14, tag_15, tag_16, tag_17, tag_18, tag_19, tag_20,
+    tag_21, tag_22, tag_23, tag_24, tag_25, tag_26, tag_27, tag_28, tag_29, tag_30,
+    tag_31, tag_32, tag_33, tag_34, tag_35, tag_36, tag_37, tag_38, tag_39, tag_40,
+    tag_41, tag_42, tag_43, tag_44, tag_45, tag_46, tag_47, tag_48, tag_49, tag_50,
+    tag_51, tag_52, tag_53, tag_54, tag_55, tag_56, tag_57, tag_58, tag_59, tag_60,
+    tag_61, tag_62, tag_63, tag_64, tag_65, tag_66, tag_67, tag_68, tag_69, tag_70,
+    tag_71, tag_72, tag_73, tag_74, tag_75, tag_76, tag_77, tag_78, tag_79, tag_80,
+    tag_81, tag_82, tag_83, tag_84, tag_85, tag_86, tag_87, tag_88, tag_89, tag_90,
+    tag_91, tag_92, tag_93, tag_94, tag_95, tag_96, tag_97, tag_98, tag_99, tag_100
+    ]
 
     for tag in tags:
         db.session.add(tag)
--- app/seeds/videos.py
+++ app/seeds/videos.py
@@ -4,14 +4,14 @@ from app.models.video import Video
 
 # Adds a demo user, you can add other users here if you want
 def seed_videos():
-    video_1 = Video(channel_id=1,
+    video_1 = Video(channel_id=15,
     title="Marvel Studios' Ant-Man and The Wasp: Quantumania | Official Trailer",
     description='''
     This February, enter the Quantum Realm. Watch the brand-new trailer for Marvel Studios' Ant-Man and The Wasp: Quantumania, only in theaters February 17, 2023.
     ''',
     video_url="https://www.youtube.com/watch?v=ZlNFpri-Y40")
 
-    video_2 = Video(channel_id=2,
+    video_2 = Video(channel_id=1,
     title="House of the Dragon (2022) | Aemond vs Lucerys Scene | S01E10",
     description='''
      An eye for an eye, a son for a son. Lucerys shall be avenged.
@@ -27,14 +27,14 @@ def seed_videos():
     ''',
     video_url="https://www.youtube.com/watch?v=Tvj-XnVKQI8&t=2s")
 
-    video_4 = Video(channel_id=4,
+    video_4 = Video(channel_id=2,
     title="Nirvana - Something In The Way (ILLENIUM Remix)",
     description='''
     My first remix in over 2 years! With my Batman jersey collab coming out and hearing this Nirvana song again in the new movie, I really wanted to try to put my spin on it. Super different vibe, had such a blast making it. Hope u guys enjoy!!
     ''',
     video_url="https://www.youtube.com/watch?v=RGNP_ZJ8Uxo&feature=youtu.be")
 
-    video_5 = Video(channel_id=1,
+    video_5 = Video(channel_id=10,
     title="Christian Bale Breaks Down His Most Iconic Characters | GQ",
     description='''
     Christian Bale breaks down a few of his most iconic characters from 'American Psycho,' 'The Dark Knight' Trilogy, 'The Fighter,' 'The Machinist,' 'The Big Short,' 'Vice,' 'Empire of the Sun,' 'Amsterdam' and 'The Pale Blue Eye.'
@@ -43,43 +43,95 @@ def seed_videos():
     ''',
     video_url="https://www.youtube.com/watch?v=TRGHIN2PGIA&t=6s")
 
-    video_6 = Video(channel_id=1,
+    video_6 = Video(channel_id=12,
     title="Stanford Computer Scientist Answers Coding Questions From Twitter | Tech Support | WIRED",
     description='''
     Chris Piech, professor of computer science at Stanford University, answers the internet's burning questions about coding. Do you need to know math to be good at coding? How many computer languages are there? Are programming and coding the same thing? How do you code A.I.? What is the meaning of the "404" error? Chris answers all these questions and much more!
     ''',
     video_url="https://www.youtube.com/watch?v=1yyRvyNQ5rQ")
 
-    video_7 = Video(channel_id=2,
+    video_7 = Video(channel_id=15,
     title="Marvel Studios' Black Panther: Wakanda Forever | Official Trailer",
     description='''
     “Show them who we are.” Watch the brand-new trailer for Marvel Studios' Black Panther: Wakanda Forever, only in theaters November 11.
     ''',
     video_url="https://www.youtube.com/watch?v=_Z3QKkl1WyM")
 
-    video_8 = Video(channel_id=3,
+    video_8 = Video(channel_id=1,
     title="Logan Roy Plays Boar On The Floor | Succession | HBO",
     description='''
    Logan Roy Plays Boar On The Floor | Succession | HBO
     ''',
     video_url="https://www.youtube.com/watch?v=dYqqW3c2mBU")
 
-    video_9 = Video(channel_id=4,
+    video_9 = Video(channel_id=11,
     title="5 mental habits they don't teach you in school",
     description='''
     In school, or really in life, we will face setbacks. When failures compound, our minds often run wild with self-doubt and hopelessness until we aren't able to continue working toward our goals. We aren't taught in school how to manage this, only that it needs to be overcome. What has helped us get past these roadblocks is following a set of principles to shift the focus from negative to positive. So here are our 5 principles, 5 mental habits to stay mentally strong and secure.
     ''',
     video_url="https://www.youtube.com/watch?v=RHHFLMaEuBM")
 
-    video_10 = Video(channel_id=1,
+    video_10 = Video(channel_id=2,
     title="In Defence of Diane | Video Essay (Bojack Horseman)",
     description='''
     People hate Diane. They call her whiny, hypocritical, and the worse person on Bojack Horseman. But I disagree.
     So as Bojack Horseman approaches its series finale (season 6, part 2), I decided to write an essay defending Diane and explaining what she does what she does.
     ''',
     video_url="https://www.youtube.com/watch?v=lk7trjqOqzo")
-    
-    videos = [video_1, video_2, video_3, video_4, video_5, video_6, video_7, video_8, video_9, video_10]
+
+    video_11 = Video(channel_id=20,
+    title="Heart-Shaped Box (VIRTU Remix)",
+    description="℗ 2018 Nice Life Recording Company and Atlantic Recording Corporation",
+    video_url="https://youtu.be/7XZNf8Mt1fY")
+
+    video_12 = Video(channel_id=19,
+    title="Jujutsu Kaisen - Hollow Purple (Ultra Epic Cover)",
+    description="Recreation cover by: MASK",
+    video_url="https://youtu.be/JZ4sjHCaIKQ")
+
+    video_13 = Video(channel_id=3,
+    title="KERR AND HARDER PARTY | Chelsea vs. Vllaznia Highlights (UEFA Women's Champions League 2022-23)",
+    description="October 26, 2022 -- Chelsea vs. Vllaznia (UEFA Women's Champions League 2022-2023 Matchday 2)",
+    video_url="https://www.youtube.com/watch?v=iPoWXwzwoKI")
+
+    video_14 = Video(channel_id=6,
+    title="Andrew Callaghan Of Channel 5 - Leftovers #30",
+    description="Thank you to Andrew of @Channel 5 with Andrew Callaghan  joining us! Check out Andrew on tour at http://channel5.news",
+    video_url="https://www.youtube.com/watch?v=aoh2W_WiuN4")
+
+    video_15 = Video(channel_id=9,
+    title="No alternative but dialogue between Germany and China: Berlin Global Advisors",
+    description="Eberhard Sandschneider, partner at Berlin Global Advisors, discusses what German Chancellor Olaf Scholz's visit to Beijing means for the economic and political landscape of both countries.",
+    video_url="https://www.youtube.com/watch?v=gclg5Zle2H0")
+
+    video_16 = Video(channel_id=17,
+    title="Monster Wolf (Full Episode) | America the Wild",
+    description="Wolves have been around for centuries, but with so many variations in the wild, what's really considered a wolf? Casey puts his tracking skills to the test to reveal the truth about wolves.",
+    video_url="https://www.youtube.com/watch?v=x_uriBZ-VQo")
+
+    video_17 = Video(channel_id=16,
+    title="Moneyball (2011) - He Gets On Base Scene (3/10) | Movieclips",
+    description="Moneyball - He Gets On Base: The scouts are doubtful when Billy (Brad Pitt) describes Peter's (Jonah Hill) strategy for picking players.",
+    video_url="https://www.youtube.com/watch?v=PlKDQqKh03Y")
+
+    video_18 = Video(channel_id=13,
+    title="What It Takes to Make 400 Bowls of Ramen From Scratch • Tasty",
+    description="Making 400 bowls of ramen from scratch is not an easy task. But for Keizo Shimamoto, ramen is his life. Watch as he artfully prepares noodles, broth, toppings, eggs, tare, and oils from start to finish - loving it every second.",
+    video_url="https://youtu.be/aagyuWlHyUI")
+
+    video_19 = Video(channel_id=4,
+    title="House of the Dragon - Otto vs Daemon - Caraxes & Syrax | Episode 2",
+    description="The Small Council learns Daemon has stolen a dragon egg and plans to take his mistress Mysaria, who he claims is pregnant, as his second wife. Otto goes to retrieve the egg, with Rhaenyra following him on her dragon, Syrax.",
+    video_url="https://youtu.be/lLbV7qxPrkY")
+
+    video_20 = Video(channel_id=7,
+    title="CALL OF DUTY MODERN WARFARE 2 Gameplay Walkthrough Part 1 Campaign FULL GAME [4K 60FPS PS5]",
+    description="Call Of Duty Modern Warfare 2 Walkthrough Part 1 and until the last part will include the full Call Of Duty Modern Warfare 2 Gameplay on PS5. This Call Of Duty Modern Warfare 2 Gameplay is recorded in 4K 60FPS on PS5 and will include the full game, all endings and all boss fights. ",
+    video_url="https://youtu.be/XV8vBqNu9nI")
+
+
+
+    videos = [video_1, video_2, video_3, video_4, video_5, video_6, video_7, video_8, video_9, video_10, video_11, video_12, video_13, video_14, video_15, video_16, video_17, video_18, video_19, video_20]
 
     for video in videos:
         db.session.add(video)
