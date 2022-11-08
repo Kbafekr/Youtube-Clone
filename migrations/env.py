fa0e007a88a01a34a354eaaffee19620ab9ca1ca
Keanen <KeanenBafekr@me.com>
2022-11-08 18:42:10 -0500

    Update env.py

--- migrations/env.py
+++ migrations/env.py
@@ -81,10 +81,6 @@ def run_migrations_online():
             **current_app.extensions['migrate'].configure_args
         )
 
-        # Create a schema (only in production)
-        if environment == "production":
-            connection.execute(f"CREATE SCHEMA IF NOT EXISTS {SCHEMA}")
-
         with context.begin_transaction():
             context.run_migrations()
 
