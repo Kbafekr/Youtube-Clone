de4bc6787cec056918f01478d36d80381869c22f
Keanen <KeanenBafekr@me.com>
2022-11-09 11:47:28 -0500

    Update notification_routes.py

--- app/api/notification_routes.py
+++ app/api/notification_routes.py
@@ -9,87 +9,87 @@ notification_routes = Blueprint('channels', __name__)
 
 # Get All notification
 @notification_routes.route('/all')
-def all_Channels():
-    all_channels = Channel.query.all()
-    if all_channels == None:
-        return "there are no channels"
-    channels = {"channels": [channels.to_dict() for channels in all_channels]}
-    return channels
+def all_notifications():
+    all_notifications = Notification.query.all()
+    if all_notifications == None:
+        return "there are no notifications"
+    notifications = {"notifications": [notification.to_dict() for notification in all_notifications]}
+    return notifications
 
 
 # get notification by id
 @notification_routes.route('/<int:id>')
 # @login_required
-def get_channel(id):
-     channel = Channel.query.get(id)
-     if channel == None:
-        return "channel is not available"
-     return channel.to_dict()
+def get_notification(id):
+     notification = Notification.query.get(id)
+     if notification == None:
+        return "notification is not available"
+     return notification.to_dict()
 
 
 #get notifications by userId
-@notification_routes.route('/<int:channel_id>/all')
+@notification_routes.route('/users/<int:user_id>/all')
 # @login_required
-def channelSubscribers(channel_id):
-    channel_subscribers = Subscriber.query.filter_by(channel_id=channel_id).all()
-    if channel_subscribers == None:
+def userNotifications(user_id):
+    user_notifications = Notification.query.filter_by(user_id=user_id).all()
+    if user_notifications == None:
         return {
-            "Message": "Channel has no subscribers",
+            "Message": "user has no notifications",
             "statusCode": "200"
         }
-    allSubscribers = {subscribers.id: subscribers.to_dict() for subscribers in channel_subscribers}
-    return allSubscribers
+    all_notifications = {notification.id: notification.to_dict() for notification in user_notifications}
+    return all_notifications
 
 
 
 # create new notification
 @notification_routes.route("/new", methods=["POST"])
 @login_required
-def create_channel():
-    form = ChannelForm()
+def create_notification():
+    form = NotificationForm()
     form['csrf_token'].data = request.cookies['csrf_token']
     if form.validate_on_submit():
         data = form.data
-        new_channel = Channel(
-            channel_name=data['channel_name'],
+        new_notification = Notification(
+            channel_id=data['channel_id'],
+            video_id=data["video_id"],
             user_id=data["user_id"],
-            profile_picture=data["profile_picture"],
-            banner_picture=data["banner_picture"]
+            is_read=data["is_read"]
         )
-        db.session.add(new_channel)
+        db.session.add(new_notification)
         db.session.commit()
-        return new_channel.to_dict()
+        return new_notification.to_dict()
     if form.errors:
         return {'errors': validation_errors_to_error_messages(form.errors)}, 401
 
 # edit notification
 @notification_routes.route("/<int:id>/edit", methods=["PUT"])
 @login_required
-def edit_channel(id):
-    form = ChannelForm()
+def edit_notification(id):
+    form = NotificationForm()
     form['csrf_token'].data = request.cookies['csrf_token']
     if form.validate_on_submit():
-        old_channel = Channel.query.get(id)
+        old_notification = Notification.query.get(id)
         data = form.data
-        old_channel.channel_name = data["channel_name"]
-        old_channel.user_id = data["user_id"]
-        old_channel.profile_picture = data["profile_picture"]
-        old_channel.banner_picture = data["banner_picture"]
+        old_notification.channel_id = data["channel_id"]
+        old_notification.video_id = data["video_id"]
+        old_notification.user_id = data["user_id"]
+        old_notification.is_read = data["is_read"]
 
         db.session.commit()
 
-        return old_channel.to_dict()
+        return old_notification.to_dict()
     if form.errors:
         return {'errors': validation_errors_to_error_messages(form.errors)}, 401
 
 # delete notification
 @notification_routes.route("/<int:id>/delete", methods=["DELETE"])
 @login_required
-def delete_image(id):
-    channel = Channel.query.get(id)
-    db.session.delete(channel)
+def delete_notification(id):
+    notification = Notification.query.get(id)
+    db.session.delete(notification)
     db.session.commit()
     return {
-    "Message": "Channel successfully deleted",
+    "Message": "notification successfully deleted",
     "statusCode": "200"
     }
